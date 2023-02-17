5f88443c273d671e2a4dd2d284daca2a6a52f906
Ben Newsome <benrogersnewsome@gmail.com>
2023-02-17 18:47:29 +0000

    Removing more dead code and readme tidying

--- readme.md
+++ readme.md
@@ -1,23 +1,40 @@
-# First Order Logic
+# Rust First Order Logic
 
-A Rust implementation of First Order Logic.
+A Rust implementation of the syntax of First Order Logic, and a logical 'harness' for making self-consistent logical assertions on a domain of discourse.
 
-Goals:
-1. Construct and manipulate logical statements using the syntax of first-order logic.
-    1. Convert logical statements into Prenex Normal Form (PNF), Skolem Normal Form (SNF) and Clause Normal Form (CNF).
-2. Define, and make assertions on, predicates and functions.
-    1. Ask logical questions about predicates and functions, to receive answers of True, False or Undetermined.
-3. Expose an interface for creating extra predicates, assertions and functions, which can be used interoperably.
+**Goals**:
+- Construct and manipulate logical statements using the syntax of first-order logic.
+- Define, and make assertions on, predicates and functions with self-consistent logical checking
+- Expose an interface for integrating packages that implement (independent of this package) higher-level mathematical concepts (such as set theory) and interfacing with them through the language of FOL.
 
-Non-goals:
+**Non-goals**:
 - Implement any formalism of set theory, or expose the idea of a set.
+- Automated theorem proving
+- Provide a base on which higher-level mathematical packages can be implemented.
 
-# Package Structure
+## Features
 
-## Syntax
+| Feature                      | Description                                                         | Status |
+| ---------------------------- | ------------------------------------------------------------------- | :----: |
+| **Logical Syntactics**       |                                                                     |        |
+| FOL grammar                  | A typed grammar for FOL                                             |   ✅   |
+| Prenex Normal Form           | A typing for PNF and conversion from others forms                   |   ✅   |
+| Skolem Normal Form           | A typing for SNF and and conversion from other forms                |   ✅   |
+| Conjunctive Normal Form      | A typing for CNF and conversion from other forms                    |   ✅   |
+| **Logical Semantics**        |                                                                     |        |
+| Predicates                   | Graph support for asserting logical predicates                      |  WIP   |
+| Functions                    | Graph support for defining logical functions                        |  WIP   |
+| Bound Variables              | Graph support for creating named bound variables                    |  WIP   |
+| Logical sentence integration | The ability to directly apply syntactic statements to the FOL graph | Future |
 
-Exposes an interface for dealing with first-order logic on a purely syntactic level, including methods for converting formulae into various normal forms.
+## Package Structure
+---
+### Syntax
 
-## Graph
+Defines a strongly-typed grammar for first-order logic, together with a set of strongly-typed normal forms, and methods for converting between them.
 
-Provides the logic which backs the semantic engine of first-order logic, using an in-memory graph structure. The graph is rarely operated on directly but is managed by the public interface created by the many default predicates and functions. 
+### Semantics
+
+Defines an in-memory graph structure that keeps track of logical statements and allows for self-consistent logical checking.
+
+The graph is rarely operated on directly but is managed by the public interface created by the many default predicates and functions.
--- src/semantics/graph.rs
+++ src/semantics/graph.rs
@@ -9,8 +9,8 @@ use super::{Arguments, ElementQuantifier, ElementSet};
 /// A signature for preventing cycles in graph traversals.
 ///
 /// Used by implementors of [Predicate] and [Function] to prevent cycles during
-/// graph traversals. The FOL graph will in general be highly cyclic.
-pub type GraphTraversalSignature = Vec<u64>;
+/// graph traversals. The FOL graph will, in general, be highly cyclic.
+pub type GraphTraversalSignature = Vec<u64>;  // Could this be a private member?
 
 ////////////////////////////////////////////////////////////////////////////////
 // Base Traits
@@ -19,9 +19,9 @@ pub type GraphTraversalSignature = Vec<u64>;
 /// A type which can be applied to elements to produce new elements.
 pub trait Function<E: Eq, const ARITY: usize> {
     /// Call the function with the specified args.
-    fn call_for_element(
+    fn call_for_args(
         &self,
-        elements: [E; ARITY],
+        args: &Arguments<ElementQuantifier<E>, ARITY>,
         sig: &mut GraphTraversalSignature,
     ) -> ElementValue<&E>;
 }
@@ -133,15 +133,17 @@ impl<T> RawGraphNode<T> {
 }
 
 impl<'a, T: 'a> RawGraphNode<T> {
+    /// SAFETY: We wrap the old inner with the new inner so that there is no
+    /// duplication. We use a creator callback so that we never expose a
+    /// dangling reference to the internal type. The use of `Fn` ensures that
+    /// the callback cannot expose the internal struct to external code.
     pub fn replace<Callback>(&self, creator: Callback)
     where
-        Callback: Fn(T) -> T,
+    Callback: Fn(T) -> T,
     {
-        // SAFETY: We wrap the old inner with the new inner so that there is no
-        // duplication. We use a creator callback so that we never expose a
-        // dangling reference to the internal type. The use of `Fn` ensures that
-        // the callback cannot expose the internal struct to external code.
         // TODO: Is this safe with the bound as an Fn?
+        // I am fairly certain it is unsafe as FnOnce or FnMut
+        // Maybe a fn(T) -> T would be better?
         unsafe {
             let inner_mut: &mut T = &mut *self._raw.get();
             let inner: T = std::ptr::read(inner_mut);
@@ -187,11 +189,17 @@ impl<E, const ARITY: usize> Predicate<E, ARITY> for PredicateNode<E, ARITY> {
     }
 
     fn get_elements_for_true(&self) -> Vec<Arguments<ElementSet<E>, ARITY>> {
+        // Safety: We use unsafe code to get a reference to the inner element,
+        // but we immediately consume the reference, and return a value which
+        // doesn't reference the inner element.
         let inner: &dyn Predicate<E, ARITY> = unsafe { self.get_inner_as_ref() }.as_ref();
         inner.get_elements_for_true()
     }
 
     fn get_elements_for_false(&self) -> Vec<Arguments<ElementSet<E>, ARITY>> {
+        // Safety: We use unsafe code to get a reference to the inner element,
+        // but we immediately consume the reference, and return a value which
+        // doesn't reference the inner element.
         let inner: &dyn Predicate<E, ARITY> = unsafe { self.get_inner_as_ref() }.as_ref();
         inner.get_elements_for_false()
     }
@@ -214,12 +222,15 @@ impl<E: Clone, const ARITY: usize> Default for PredicateNode<E, ARITY> {
 pub type FunctionNode<E, const ARITY: usize> = GraphNode<Box<dyn Function<E, ARITY>>>;
 
 impl<E: Eq, const ARITY: usize> Function<E, ARITY> for FunctionNode<E, ARITY> {
-    fn call_for_element(
+    fn call_for_args(
         &self,
-        elements: [E; ARITY],
+        args: &Arguments<ElementQuantifier<E>, ARITY>,
         sig: &mut GraphTraversalSignature,
     ) -> ElementValue<&E> {
+        // Safety: We use unsafe code to get a reference to the inner element,
+        // but we immediately consume the reference, and return a value which
+        // doesn't reference the inner element.
         let inner = unsafe { self.get_inner_as_ref() }.as_ref();
-        inner.call_for_element(elements, sig)
+        inner.call_for_args(args, sig)
     }
 }
